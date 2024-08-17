9635aa8a187f9e18a1193c34fa63c787a8132440
kazi fahim <kazifahim661@gmail.com>
2024-08-17 11:58:58 +0600

    data sort implement

--- src/private/Private.jsx
+++ src/private/Private.jsx
@@ -6,7 +6,6 @@ const Private = ({ children }) => {
     const { user, loading } = useContext(AuthContext)
     const location = useLocation();
 
-
     if (loading && !user) {
         return <div className="flex justify-center items-center pt-72"><span className="loading loading-spinner loading-lg"></span></div>
     }
